5be3f5d1ad0288534f4a52eb1b298ea43e23a9a9
Nikola Marić <nikola.maric@infobip.com>
2015-10-08 11:19:52 +0200

    Using the same authentication form as on advancedSms.php.

--- logsRequestForm.php
+++ logsRequestForm.php
@@ -5,13 +5,14 @@
 <body>
 
 <form action="logs.php" method="POST">
-	Username:<br>
-    <input type="text" name="username">
-    <br>
-	Password:<br>
-    <input type="password" name="password">
-    <br><br>
-    
+	<span style="color:#FF0000"><b>Authentication</b></span><br>
+	<label for="in_username">Username:</label><br>
+	<input id="in_username" type="text" name="username" title="Username">
+	<br>
+	<label for="in_password">Password:</label><br>
+	<input id="in_password" type="password" name="password" title="Password">
+	<hr>
+	<br>
 	<input type="submit" value="Get message logs">
 </form>
 
